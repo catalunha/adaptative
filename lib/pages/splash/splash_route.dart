d979405d955897346675f04f7296c5a78c70ee06
catalunha <catalunha.mj@gmail.com>
2023-11-16 13:19:56 -0300

    rev

--- lib/pages/splash/splash_route.dart
+++ lib/pages/splash/splash_route.dart
@@ -3,24 +3,8 @@ import 'dart:developer';
 import 'package:adaptative/pages/splash/controller/controllers.dart';
 import 'package:adaptative/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
-
-// class SplashRoute {
-//   Widget page(BuildContext context) {
-//     log('SplashRoute.page');
-
-//     return MultiBlocProvider(
-//       providers: [
-//         BlocProvider<SplashPageController>(
-//           create: (BuildContext context2) => SplashPageController(),
-//         )
-//       ],
-//       child: SplashPage(splashPageController: context.read<SplashPageController>()),
-//       // child: SplashPage(splashPageController: SplashPageController()),
-//       // child: const SplashPage2(),
-//     );
-//   }
-// }
 
 class SplashRoute {
   Widget page(BuildContext context) {
@@ -33,7 +17,30 @@ class SplashRoute {
         )
       ],
       builder: (context, child) => SplashPage(
-          splashPageController: context.read<SplashPageController>()),
+        splashPageController: context.read<SplashPageController>(),
+      ),
     );
   }
 }
+
+// class SplashRoute {
+//   Widget page(BuildContext context) {
+//     log('SplashRoute.page');
+
+//     return MultiBlocProvider(
+//       providers: [
+//         BlocProvider<SplashPageController>(
+//           create: (BuildContext context) => SplashPageController(),
+//         )
+//       ],
+//       child: Builder(
+//         builder: (context) {
+//           return SplashPage(
+//               splashPageController: context.read<SplashPageController>());
+//         },
+//       ),
+//       // child: SplashPage(splashPageController: SplashPageController()),// gera rebuild
+//       // child: const SplashPage2(),
+//     );
+//   }
+// }
