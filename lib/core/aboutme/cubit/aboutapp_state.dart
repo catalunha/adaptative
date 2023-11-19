f9eb9bfc8fa1d0c80ecd1e8a2a23bbee781de9fa
catalunha <catalunha.mj@gmail.com>
2023-11-19 08:19:13 -0300

    rev

--- lib/core/aboutme/cubit/aboutapp_state.dart
+++ lib/core/aboutme/cubit/aboutapp_state.dart
@@ -4,11 +4,8 @@ enum LayoutSize { small, medium, large, undefined }
 
 @freezed
 class AboutappState with _$AboutappState {
-  const factory AboutappState.initial() = AboutappStateInitial;
-  const factory AboutappState.loading() = AboutappStateLoading;
-  const factory AboutappState.loaded({
+  const factory AboutappState({
     @Default(LayoutSize.undefined) LayoutSize layoutSize,
     @Default(0.0) double maxWidth,
   }) = AboutappStateLoaded;
-  const factory AboutappState.error({String? error}) = AboutappStateError;
 }
--- lib/pages/todo/insert/controller/states.dart
+++ lib/pages/todo/insert/controller/states.dart
@@ -2,6 +2,13 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'states.freezed.dart';
 
+// extension WeatherStatusX on WeatherStatus {
+//   bool get isInitial => this == WeatherStatus.initial;
+//   bool get isLoading => this == WeatherStatus.loading;
+//   bool get isSuccess => this == WeatherStatus.success;
+//   bool get isFailure => this == WeatherStatus.failure;
+// }
+
 @freezed
 sealed class TaskState with _$TaskState {
   const factory TaskState.initial() = TaskStateInitial;
