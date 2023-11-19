ba217c2311e0a72d38a7e61e857ed518f80008cf
catalunha <catalunha.mj@gmail.com>
2023-11-18 21:03:33 -0300

    rev

--- lib/core/aboutme/cubit/aboutapp_cubit.dart
+++ lib/core/aboutme/cubit/aboutapp_cubit.dart
@@ -5,5 +5,10 @@ part 'aboutapp_state.dart';
 part 'aboutapp_cubit.freezed.dart';
 
 class AboutappCubit extends Cubit<AboutappState> {
-  AboutappCubit() : super(AboutappState.initial());
+  AboutappCubit() : super(const AboutappState.initial());
+
+  void setLayoutSize(LayoutSize value) {
+    emit(const AboutappStateLoading());
+    // emit(AboutappStateLoaded(layoutSize: value));
+  }
 }
--- lib/core/aboutme/cubit/aboutapp_state.dart
+++ lib/core/aboutme/cubit/aboutapp_state.dart
@@ -1,9 +1,14 @@
 part of 'aboutapp_cubit.dart';
 
+enum LayoutSize { small, medium, large, undefined }
+
 @freezed
 class AboutappState with _$AboutappState {
   const factory AboutappState.initial() = AboutappStateInitial;
   const factory AboutappState.loading() = AboutappStateLoading;
-  const factory AboutappState.loaded() = AboutappStateLoaded;
+  const factory AboutappState.loaded({
+    @Default(LayoutSize.undefined) LayoutSize layoutSize,
+    @Default(0.0) double maxWidth,
+  }) = AboutappStateLoaded;
   const factory AboutappState.error({String? error}) = AboutappStateError;
 }
